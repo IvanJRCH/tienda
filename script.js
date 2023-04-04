2acf92e1f96c4a71913f56b63a3c15c9fa297e0b
IvanJRCH <thisivan@hotmail.com>
2023-04-04 16:01:50 -0400

    menus
    menus

---
+++ script.js
@@ -0,0 +1,9 @@
+
+window.addEventListener('DOMContentLoaded',function() {
+
+	var lBoton = [];
+	var o = document.getElemetById("pages");
+	var ll = o.getElementByTagName("li");
+	alert();
+
+},false);
